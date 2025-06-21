b53d4f8f9992700c1e4f61c5b03299a78b7a6398
hayato2158 <k.hayato2158@gmail.com>
2025-06-21 10:14:10 +0900

    ADD：メインプロセスの実装

---
+++ src/main/ipc/useNotes.ts
@@ -0,0 +1,144 @@
+import { dialog, ipcMain } from 'electron'
+import { v7 as uuidv7 } from 'uuid'
+
+/** repository */
+import {
+  readNotesInfo,
+  saveNoteInfo,
+  getNoteInfo,
+  writeNoteInfo,
+  deleteNoteInfo
+} from '@main/repository/noteInfoRepository'
+
+/** utils */
+import { handleError } from '@main/utils/handler'
+import { logger } from '@main/utils/logger'
+
+/** enum */
+import {
+  LOG_LEVEL,
+  DIALOG_TYPE,
+  DialogValue,
+  WELCOME,
+  DIALOG_CANCEL_ID,
+  DIALOG_DEFAULT_ID,
+  INFO_MASSAGE
+} from '@main/contents/enum'
+
+/** types */
+import { GetNote, NoteContent, NoteInfo } from '@main/contents/ipc'
+
+/**
+ * 全ファイルの取得
+ */
+ipcMain.handle('getNotes', async (): Promise<ReturnType<GetNote>> => {
+  const [readNotes, readNotesError] = await handleError(readNotesInfo())
+
+  if (readNotesError) {
+    logger(LOG_LEVEL.ERROR, `ensureDir Error: ${readNotesError}`)
+  }
+
+  if (!readNotes?.length) {
+    logger(LOG_LEVEL.INFO, INFO_MASSAGE.NO_NOTE_FOUND)
+
+    const newNote: NoteInfo[] = [
+      {
+        uuid: uuidv7(),
+        title: '新規ノート',
+        content: WELCOME.WELCOME_NOTE_CONTENT,
+        lastEditTime: new Date() // デフォルトの最終編集日時
+      }
+    ]
+    const [_, saveNoteError] = await handleError(saveNoteInfo(newNote[0]))
+
+    if (saveNoteError) {
+      logger(LOG_LEVEL.ERROR, `saveNoteInfo Error: ${saveNoteError}`)
+    }
+    return newNote
+  }
+  return readNotes
+})
+
+/**
+ * ファイル読み込み
+ */
+ipcMain.handle('readNote', async (_, uuid: string): Promise<NoteContent> => {
+  const [getNote, getNoteError] = await handleError(getNoteInfo(uuid))
+  if (getNoteError) {
+    logger(LOG_LEVEL.ERROR, `readNote Error: ${getNoteError}`)
+    return
+  }
+  return getNote?.content
+})
+
+/**
+ * ファイル書き込み
+ */
+ipcMain.handle('writeNote', async (_c, note: NoteInfo): Promise<void> => {
+  /** contentの一行目をtitle表示に設定する。 */
+  const noteTitle = note.content?.slice(0, note.content.indexOf('\n'))
+  const noteWithTitle = {
+    ...note,
+    title: noteTitle ?? ''
+  }
+
+  const [_, writeFileError] = await handleError(writeNoteInfo(noteWithTitle))
+
+  if (writeFileError) {
+    logger(LOG_LEVEL.ERROR, `writeNote Error: ${writeFileError}`)
+    return
+  }
+})
+
+/**
+ * ファイル新規作成
+ */
+ipcMain.handle(
+  'createNote',
+  async (_, filename: string = '新規ノート'): Promise<NoteInfo | undefined> => {
+    const newNote: NoteInfo = {
+      uuid: uuidv7(),
+      title: filename,
+      content: WELCOME.WELCOME_NOTE_CONTENT,
+      lastEditTime: new Date() // デフォルトの最終編集日時
+    }
+    const [__, saveNoteError] = await handleError(saveNoteInfo(newNote))
+
+    if (saveNoteError) {
+      logger(LOG_LEVEL.ERROR, `saveNoteInfo Error: ${saveNoteError}`)
+    }
+
+    logger(LOG_LEVEL.INFO, `saving note: ${filename}`)
+
+    return newNote
+  }
+)
+
+/**
+ * ファイル削除
+ */
+ipcMain.handle('deleteNote', async (_, filename: string, uuid: string): Promise<boolean> => {
+  const { response } = await dialog.showMessageBox({
+    type: DIALOG_TYPE.WARNING as DialogValue,
+    title: 'ノート削除',
+    message: `${filename} を削除しますか?`,
+    buttons: ['削除', 'キャンセル'], // 0：削除, 1：キャンセル
+    defaultId: DIALOG_DEFAULT_ID,
+    cancelId: DIALOG_CANCEL_ID
+  })
+
+  if (response === DIALOG_CANCEL_ID) {
+    console.info(INFO_MASSAGE.NOTE_CANCELED)
+    return false
+  }
+
+  const [__, deleteFileError] = await handleError(deleteNoteInfo(uuid))
+
+  if (deleteFileError) {
+    logger(LOG_LEVEL.ERROR, `DeleteNote Error: ${deleteFileError}`)
+    return false
+  }
+  logger(LOG_LEVEL.INFO, `Deleting note: ${filename}`)
+
+  return true
+})
--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,22 +1,53 @@
-import { contextBridge } from 'electron'
-import { electronAPI } from '@electron-toolkit/preload'
+import { contextBridge, ipcRenderer } from 'electron'
 
-// Custom APIs for renderer
-const api = {}
+/** type */
+import { ERROR_MASSAGE } from '../main/contents/enum'
+import { CreateNote, DeleteNote, GetNote, NoteInfo, ReadNote, WriteNote } from '@main/contents/ipc'
 
-// Use `contextBridge` APIs to expose Electron APIs to
-// renderer only if context isolation is enabled, otherwise
-// just add to the DOM global.
-if (process.contextIsolated) {
-  try {
-    contextBridge.exposeInMainWorld('electron', electronAPI)
-    contextBridge.exposeInMainWorld('api', api)
-  } catch (error) {
-    console.error(error)
-  }
-} else {
-  // @ts-ignore (define in dts)
-  window.electron = electronAPI
-  // @ts-ignore (define in dts)
-  window.api = api
+if (!process.contextIsolated) {
+  // コンテキストが分離されていない場合
+  throw new Error(ERROR_MASSAGE.MUST_USE_CONTEXT_ISOLATION)
+}
+
+const getNotes = async (): Promise<ReturnType<GetNote>> => {
+  return await ipcRenderer.invoke('getNotes')
+}
+
+const createNote = async (filename: string): Promise<ReturnType<CreateNote>> => {
+  return ipcRenderer.invoke('createNote', filename)
+}
+
+const readNote = async (uuid: string): Promise<ReturnType<ReadNote>> => {
+  return ipcRenderer.invoke('readNote', uuid)
+}
+
+const writeNote = async (note: NoteInfo): Promise<ReturnType<WriteNote>> => {
+  return ipcRenderer.invoke('writeNote', note)
+}
+
+const deleteNote = async (filename: string, uuid: string): Promise<ReturnType<DeleteNote>> => {
+  return ipcRenderer.invoke('deleteNote', filename, uuid)
+}
+
+// Docs: https://www.electronjs.org/docs/latest/tutorial/context-isolation/#usage-with-typescript
+// contextBridgeに露出させるAPIを定義
+export interface IElectronAPI {
+  getNotes: typeof getNotes
+  createNote: typeof createNote
+  readNote: typeof readNote
+  writeNote: typeof writeNote
+  deleteNote: typeof deleteNote
+}
+
+try {
+  // Docs: https://electronjs.org/docs/api/context-bridge
+  contextBridge.exposeInMainWorld('electron', {
+    getNotes,
+    createNote,
+    readNote,
+    writeNote,
+    deleteNote
+  })
+} catch (error) {
+  console.error(error)
 }
